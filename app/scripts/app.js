664f1c45b27ce2a005e8a2cfd5127637a8816a74
Hernan Humaña <Hernan@MacBookH.local>
2017-02-06 23:54:49 -0300

    Primer codigo

---
+++ app/scripts/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name hernanhApp
+ * @description
+ * # hernanhApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('hernanhApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider,$locationProvider) {
+
+     $locationProvider.hashPrefix('');
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+
+      
+  });
